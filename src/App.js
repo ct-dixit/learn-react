5d7fe6c72b9cbc75d5be7857116e71ae63cfb775
ct-dixit <106728438+ct-dixit@users.noreply.github.com>
2023-03-15 18:18:59 +0530

    prettier formate

--- src/App.js
+++ src/App.js
@@ -1,64 +1,67 @@
-import React, { useState } from 'react';
-import './App.css';
-import Alert from './component/Alert';
-import About from './component/About';
-import Navbar from './component/Navbar';
-import TextForm from './component/TextForm';
+import React, { useState } from "react";
+import "./App.css";
+import Alert from "./component/Alert";
+import About from "./component/About";
+import Navbar from "./component/Navbar";
+import TextForm from "./component/TextForm";
 
 import {
   BrowserRouter as Router,
-  Switch,
   Route,
 } from "react-router-dom";
 
-
 function App() {
-
-  const [mode, setMode] = useState('light')
-  const [alert, setAlert] = useState(null)  // object
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null); // object
 
   const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type: type
-    })
+      type: type,
+    });
     setTimeout(() => {
-      setAlert(null)
-    }, 1000)
-  }
+      setAlert(null);
+    }, 1000);
+  };
 
   const toggleMode = () => {
-    if (mode === 'light') {
-      setMode('dark')
-      document.body.style.backgroundColor = 'grey';
-      showAlert("Dark Mode has been Enabled", "success")
-      document.title = 'New App'
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "grey";
+      showAlert("Dark Mode has been Enabled", "success");
+      document.title = "New App";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode has been Enabled", "success");
+      document.title = "New React App";
     }
-    else {
-      setMode('light')
-      document.body.style.backgroundColor = 'white';
-      showAlert("Light Mode has been Enabled", "success")
-      document.title = 'New React App'
-    }
-  }
+  };
 
   return (
     <>
       <Router>
-        <Navbar title="Berry" aboutText="About" mode={mode} toggleMode={toggleMode} />
+        <Navbar
+          title="Berry"
+          aboutText="About"
+          mode={mode}
+          toggleMode={toggleMode}
+        />
         <Alert alert={alert} />
         <div className="container my-3">
-           <Switch>
           <Route path="/about">
             <About />
           </Route>
           <Route path="/">
-          <TextForm showAlert={showAlert} heading="Enter The Text to Analyze Below" mode={mode} />
+          <TextForm
+                showAlert={showAlert}
+                heading="Enter The Text to Analyze Below"
+                mode={mode}
+              />
           </Route>
-        </Switch>
         </div>
       </Router>
-      </>
+    </>
   );
 }
 
--- src/component/Alert.js
+++ src/component/Alert.js
@@ -1,26 +1,27 @@
-import React from 'react'
+import React from "react";
 
 function Alert(props) {
+  // FIRST LATER CAPITALIZE
 
-    // FIRST LATER CAPITALIZE
-
-    const capitalize = (word) => {
-        const lower = word.toLowerCase();
-        return lower.charAt(0).toUpperCase() + lower.slice(1);
-    }
-    return (
-            props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-                <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
-                {/* <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> */}
-            </div>
+  const capitalize = (word) => {
+    const lower = word.toLowerCase();
+    return lower.charAt(0).toUpperCase() + lower.slice(1);
+  };
+  return (
+    props.alert && (
+      <div
+        className={`alert alert-${props.alert.type} alert-dismissible fade show`}
+        role="alert"
+      >
+        <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
+        {/* <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> */}
+      </div>
     )
+  );
 }
 
-export default Alert
-
-
+export default Alert;
 
 // her props.alert && some item
-
 
 // here props.alert false so some item not show and true to show some item
